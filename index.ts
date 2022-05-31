49d0cd8ae2400c42e4097b93d4b13d47e974dc7b
Tomasz Buras <tomaszburas.dev@gmail.com>
2022-05-31 17:16:37 +0200

    Added handle errors

--- index.ts
+++ index.ts
@@ -2,11 +2,12 @@ import express, { json } from 'express';
 import 'express-async-errors';
 import cors from 'cors';
 import { PORT } from './config';
+import { handleError } from './utils/handleErrors';
 
 const app = express();
 
 app.use(cors());
 app.use(json());
 
-// app.use(handleError);
+app.use(handleError);
 app.listen(PORT, () => `Server listening on port ${PORT}`);
