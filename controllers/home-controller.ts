405da45f86a2a1015c81e16b6d2d18c8f80c4c90
Tomasz Buras <tomaszburas.dev@gmail.com>
2022-06-01 10:48:27 +0200

    Added home-controller.

--- controllers/home-controller.ts
+++ controllers/home-controller.ts
@@ -1,9 +1,46 @@
 import { Request, Response } from 'express';
+import { ACCESS_TOKEN } from '../config';
 
 export class HomeController {
-    static homePage(req: Request, res: Response) {}
+    static homePage(req: Request, res: Response) {
+        res.status(200).json({
+            success: true,
+            message: 'Welcome :)',
+        });
+    }
 
-    static register(req: Request, res: Response) {}
+    static async register(req: Request, res: Response) {
+        const { username, password } = req.body;
 
-    static login(req: Request, res: Response) {}
+        const user = new UserRecord({
+            username,
+            password,
+        });
+
+        await user.create();
+
+        res.status(201).json({ success: true });
+    }
+
+    static login(req: Request, res: Response) {
+        const { username, password } = req.body;
+
+        const user = await AdminRecord.login(name, password);
+
+        const payload = {
+            _id: user._id,
+            name: user.name,
+        };
+
+        const token = jwt.sign(payload, ACCESS_TOKEN, { expiresIn: '1d' });
+
+        res.status(200)
+            .cookie('access_token', token, {
+                maxAge: 24 * 60 * 60 * 1000,
+                httpOnly: true,
+            })
+            .json({
+                success: true,
+            });
+    }
 }
