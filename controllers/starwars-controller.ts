77cde9916626deba60a278d4e256a7eac9b92ecc
Tomasz Buras <tomaszburas.dev@gmail.com>
2022-06-01 13:47:52 +0200

    Added getAll people from SWAPI.

--- controllers/starwars-controller.ts
+++ controllers/starwars-controller.ts
@@ -1,7 +1,24 @@
+import fetch from 'node-fetch';
 import { Request, Response } from 'express';
 
 export class StarWarsController {
-    static getAll(req: Request, res: Response) {}
+    static async getAll(req: Request, res: Response) {
+        const data = await fetch('https://swapi.dev/api/people');
+        const people = await data.json();
+
+        const allPeople = [];
+        const pages = Math.ceil(people.count / people.results.length);
+
+        for (let i = 1; i <= pages; i++) {
+            const resultPerPage = await fetch(`https://swapi.dev/api/people/?page=${i}`);
+            const peoplePerPage = await resultPerPage.json();
+            allPeople.push(peoplePerPage.results);
+        }
+
+        res.status(200).json({
+            allPeople,
+        });
+    }
 
     static getFiltered(req: Request, res: Response) {}
 }
