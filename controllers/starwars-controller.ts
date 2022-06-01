d2745a872812cee1f9dc79fd6bbc94fe67779b85
Tomasz Buras <tomaszburas.dev@gmail.com>
2022-06-01 20:12:44 +0200

    Added filtered people (swapi).

--- controllers/starwars-controller.ts
+++ controllers/starwars-controller.ts
@@ -1,33 +1,39 @@
-import fetch from 'node-fetch';
 import { Request, Response } from 'express';
+import { getAllPeopleSwapi } from '../utils/get-all-people-swapi';
 import { ValidationError } from '../utils/handle-errors';
 
 export class StarWarsController {
     static async getAll(req: Request, res: Response) {
-        const data = await fetch('https://swapi.dev/api/people');
-        const people = await data.json();
-
-        if (!people.results.length) {
-            throw new ValidationError('People not found.');
-        }
-
-        const allPeople = [];
-        allPeople.push(...people.results);
-
-        const pages = Math.ceil(people.count / people.results.length);
-
-        if (pages > 1) {
-            for (let i = 2; i <= pages; i++) {
-                const resultPerPage = await fetch(`https://swapi.dev/api/people/?page=${i}`);
-                const peoplePerPage = await resultPerPage.json();
-                allPeople.push(...peoplePerPage.results);
-            }
-        }
-
         res.status(200).json({
-            people: allPeople,
+            success: true,
+            people: await getAllPeopleSwapi(),
         });
     }
 
-    static getFiltered(req: Request, res: Response) {}
+    static async getFiltered(req: Request, res: Response) {
+        if (!req.query) {
+            throw new ValidationError('Query is empty.');
+        }
+
+        const queries = Object.entries(req.query);
+
+        const people = await getAllPeopleSwapi();
+
+        const filtered = people.filter((person) => {
+            let result = true;
+            queries.map(([key, value]) => {
+                if (person[key] !== value) {
+                    result = false;
+                    return;
+                }
+            });
+
+            return result ? people : false;
+        });
+
+        res.status(200).json({
+            success: true,
+            people: filtered ? filtered : [],
+        });
+    }
 }
