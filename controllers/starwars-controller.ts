9668f0112825a6884ca51c4c44cd684382e96756
Tomasz Buras <tomaszburas.dev@gmail.com>
2022-06-01 13:58:20 +0200

    Fixed getAll people from SWAPI (added validation).

--- controllers/starwars-controller.ts
+++ controllers/starwars-controller.ts
@@ -1,22 +1,31 @@
 import fetch from 'node-fetch';
 import { Request, Response } from 'express';
+import { ValidationError } from '../utils/handle-errors';
 
 export class StarWarsController {
     static async getAll(req: Request, res: Response) {
         const data = await fetch('https://swapi.dev/api/people');
         const people = await data.json();
 
+        if (!people.results.length) {
+            throw new ValidationError('People not found.');
+        }
+
         const allPeople = [];
+        allPeople.push(...people.results);
+
         const pages = Math.ceil(people.count / people.results.length);
 
-        for (let i = 1; i <= pages; i++) {
-            const resultPerPage = await fetch(`https://swapi.dev/api/people/?page=${i}`);
-            const peoplePerPage = await resultPerPage.json();
-            allPeople.push(peoplePerPage.results);
+        if (pages > 1) {
+            for (let i = 2; i <= pages; i++) {
+                const resultPerPage = await fetch(`https://swapi.dev/api/people/?page=${i}`);
+                const peoplePerPage = await resultPerPage.json();
+                allPeople.push(...peoplePerPage.results);
+            }
         }
 
         res.status(200).json({
-            allPeople,
+            people: allPeople,
         });
     }
 
