fd3a272e3705c2bba67b8298aada3fa50606c520
Tomasz Buras <tomaszburas.dev@gmail.com>
2022-06-02 22:42:32 +0200

    Added database file.

---
+++ zad_rek.sql
@@ -0,0 +1,55 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               10.4.21-MariaDB - mariadb.org binary distribution
+-- Server OS:                    Win64
+-- HeidiSQL Version:             12.0.0.6468
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+-- Dumping database structure for zad_rek
+CREATE DATABASE IF NOT EXISTS `zad_rek` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */;
+USE `zad_rek`;
+
+-- Dumping structure for table zad_rek.users
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` varchar(36) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(254) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`email`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table zad_rek.weathers
+CREATE TABLE IF NOT EXISTS `weathers` (
+  `id` varchar(36) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `date` datetime NOT NULL,
+  `place` varchar(190) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `country` varchar(90) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `coordLat` decimal(10,7) NOT NULL,
+  `coordLon` decimal(10,7) NOT NULL,
+  `temp` decimal(4,2) NOT NULL,
+  `pressure` smallint(6) NOT NULL DEFAULT 0,
+  `weather` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `weatherDesc` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `windSpeed` decimal(5,2) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Data exporting was unselected.
+
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
