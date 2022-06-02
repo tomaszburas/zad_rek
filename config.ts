c99b1e40bc48b09ede5206358e8bad132a8afc2e
Tomasz Buras <tomaszburas.dev@gmail.com>
2022-06-02 13:33:23 +0200

    Updated data.

--- config.ts
+++ config.ts
@@ -2,9 +2,11 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const PORT = process.env.PORT || 3001;
-export const ACCESS_TOKEN = process.env.ACCESS_TOKEN || 'Random String';
+export const ACCESS_TOKEN =
+    process.env.ACCESS_TOKEN ||
+    'sdfdsafdsfdsACR$3$#@C$#%@ cV54v354VY345 v3Y3v54Y%#^vy56yv65f345th^%jh^&5J&^&^&IJ hsdfFDSA3322';
 export const DB_HOST = process.env.DB_HOST || 'localhost';
 export const DB_USER = process.env.DB_USER || 'root';
 export const DB_NAME = process.env.DB_NAME || 'zad_rek';
 export const DB_PASSWORD = process.env.DB_PASSWORD || '';
-export const WEATHER_API_KEY = process.env.WEATHER_API_KEY;
+export const WEATHER_API_KEY = process.env.WEATHER_API_KEY || 'fff34e39432768cd2c730f7793b920f4';
